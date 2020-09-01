0ce6a74cb87d56e878a89f2aaedc7574cc94793a
victor <1103231037@qq.com>
2020-09-01 16:48:55 +0800

    add

--- mapbox相关/vue-mapbox-例子和文档/src/App.vue
+++ mapbox相关/vue-mapbox-例子和文档/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div class="page">
       <div class="header">
-        <router-link to="/">Home</router-link>
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>
       </div>
       <router-view />
     </div>
